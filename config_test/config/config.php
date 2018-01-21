39fc2ca2bc98b9d66e3409fbbfbe424a326e9542
Álvaro Castellano Vela <alvaro.castellano.vela@gmail.com>
2018-01-22 00:43:04 +0100

    Config file left

---
+++ config_test/config/config.php
@@ -0,0 +1,44 @@
+<?php
+$CONFIG = array (
+  'instanceid' => 'test',
+  'passwordsalt' => 'test',
+  'secret' => 'test',
+  'trusted_domains' =>
+  array (
+    0 => 'test',
+  ),
+  'datadirectory' => 'test',
+  'overwrite.cli.url' => 'test',
+  'dbtype' => 'mysql',
+  'version' => 'test',
+  'dbname' => 'testestt',
+  'dbhost' => 'localhost',
+  'dbport' => '',
+  'dbtableprefix' => 'oc_',
+  'dbuser' => 'test',
+  'dbpassword' => 'test',
+  'installed' => true,
+  'memcache.local' => '\\OC\\Memcache\\Memcached',
+  'memcache.distributed' => '\\OC\\Memcache\\Memcached',
+  'memcached_servers' =>
+  array (
+    0 =>
+    array (
+      0 => 'localhost',
+      1 => 11211,
+    ),
+  ),
+  'mail_smtpmode' => 'test',
+  'mail_smtpauthtype' => 'test',
+  'mail_from_address' => 'test',
+  'mail_domain' => 'test',
+  'mail_smtphost' => 'test',
+  'mail_smtpport' => 'test',
+  'mail_smtpauth' => 1,
+  'mail_smtpname' => 'test',
+  'mail_smtppassword' => 'test',
+  'mail_smtpsecure' => 'test',
+  'maintenance' => false,
+  'theme' => '',
+  'loglevel' => 2,
+);
---
+++ config_test_no_values/config/config.php
@@ -0,0 +1,39 @@
+<?php
+$CONFIG = array (
+  'instanceid' => 'test',
+  'passwordsalt' => 'test',
+  'secret' => 'test',
+  'trusted_domains' =>
+  array (
+    0 => 'test',
+  ),
+  'datadirectory' => 'test',
+  'overwrite.cli.url' => 'test',
+  'dbtype' => 'mysql',
+  'version' => 'test',
+  'dbtableprefix' => 'oc_',
+  'installed' => true,
+  'memcache.local' => '\\OC\\Memcache\\Memcached',
+  'memcache.distributed' => '\\OC\\Memcache\\Memcached',
+  'memcached_servers' =>
+  array (
+    0 =>
+    array (
+      0 => 'localhost',
+      1 => 11211,
+    ),
+  ),
+  'mail_smtpmode' => 'smtp',
+  'mail_smtpauthtype' => 'LOGIN',
+  'mail_from_address' => 'test',
+  'mail_domain' => 'test',
+  'mail_smtphost' => 'test',
+  'mail_smtpport' => '111',
+  'mail_smtpauth' => 1,
+  'mail_smtpname' => 'test',
+  'mail_smtppassword' => 'test',
+  'mail_smtpsecure' => 'ssl',
+  'maintenance' => false,
+  'theme' => '',
+  'loglevel' => 2,
+);
