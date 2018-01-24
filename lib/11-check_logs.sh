1e5dc8517f6900289479d184634aabfc9812962a
Álvaro Castellano Vela <alvaro.castellano.vela@gmail.com>
2018-01-24 17:55:21 +0100

    Refactor done, now some unit test won't need to call the entire script

---
+++ lib/11-check_logs.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+#
+# **  nextcloud_backups_aws_s3   **
+# **         check logs          **
+#
+# Utility to make backups of Nextcloud and store them in an S3 bucket
+# Check if logging into external file is enabled
+#
+# Álvaro Castellano Vela - https://github.com/a-castellano
+
+# Logger
+source lib/04-logger.sh
+
+function check_log_file {
+if [[ "$ENABLE_LOG" = true ]]; then
+    if [[ "$ENABLE_LOG" = true && -z $LOG_FILE ]];then
+        error_msg="Log enabled but there is no log file declared, add '--log-file' option."
+        report_error $error_msg
+        exit 1
+    else
+        # Check if folder is writable
+        if [ -f "$LOG_FILE" ]; then
+            if [[ ! -w "$LOG_FILE" ]];then
+                error_msg="Cannot write log in '$LOG_FILE': Permission Denied"
+                report_error $error_msg
+                exit 1
+            fi
+        else
+            folder=$( $ECHO $LOG_FILE | $SED 's|/[^/]*$||' )
+            if [[ -w $folder ]]; then
+                $TOUCH $LOG_FILE
+            else
+                if [[ -d $folder  ]]; then
+                    error_msg="Cannot write log in '$folder': Permission Denied"
+                    report_error $error_msg
+                    exit 1
+                else
+                    error_msg="Cannot write log in '$folder': Given path does not exist"
+                    report_error $error_msg
+                    exit 1
+                fi
+            fi
+        fi
+    fi
+fi
+}
+
---
+++ lib/12-check_required_variables.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+#
+# **  nextcloud_backups_aws_s3   **
+# **  check required variables   **
+#
+# Utility to make backups of Nextcloud and store them in an S3 bucket
+# Check if all required varible for running the script are set
+#
+# Álvaro Castellano Vela - https://github.com/a-castellano
+
+# Logger
+source lib/04-logger.sh
+
+function check_required_variables {
+    declare -a required_variables
+    required_variables=(
+        's3-access-key'
+        's3-secret-key'
+        's3-bucket'
+        'database-name'
+        'database-user'
+        'database-passwd'
+        'database-host'
+        'database-port'
+        'nextcloud-path'
+        'nextcloud-users'
+    )
+    
+    unset_variables=""
+    for var in ${required_variables[@]}
+    do
+        variable=$($ECHO $var | $TR '[:lower:]' '[:upper:]' | $TR '-' '_' )
+        if [[ -z ${!variable} ]]; then
+            unset_variables="$unset_variables, $var"
+        fi
+    done
+    if [[ ! "$unset_variables" = "" ]]; then
+        unset_variables=$($ECHO $unset_variables | $SED 's/,//' )
+        error_msg="The following variables has to have value: $unset_variables. Please set a valid value for it."
+        report_error $error_msg
+        usage
+        exit 1
+    fi
+}
