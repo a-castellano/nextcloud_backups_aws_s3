8464590b19ff4c57d7d019cbb5a917bea2839f1c
Álvaro Castellano Vela <alvaro.castellano.vela@gmail.com>
2018-01-31 06:37:58 +0100

    Validating exclude-database param

--- lib/10-set_unset_variables.sh
+++ lib/10-set_unset_variables.sh
@@ -11,17 +11,25 @@
 function set_unset_variables {
     if [[ -z $DATABASE_PORT ]]; then
         DATABASE_PORT=3306
+        write_log "DATABASE_PORT has been set to its default value $DATABASE_PORT."
     fi
 
     if [[ -z $DATABASE_HOST ]]; then
         DATABASE_HOST="localhost"
+        write_log "DATABASE_HOST has been set to its default value $DATABASE_HOST."
     fi
 
     if [[ -z $HTTP_USER ]]; then
         HTTP_USER="www-data"
+        write_log "HTTP_USER has been set to its default value $HTTP_USER."
     fi
 
     if [[ -z $NEXTCLOUD_USERS ]]; then
         NEXTCLOUD_USERS='ALL'
+        write_log "NEXTCLOUD_USERS has been set to its default value $NEXTCLOUD_USERS."
+    fi
+
+    if [[ -z $EXCLUDE_DATABASE ]]; then
+       EXCLUDE_DATABASE=false
     fi
 }
